35ae2aa8f7dadc98918f162f3bad5fc55ecab398
Jhay-Great <alemonai.jenef@gmail.com>
2023-12-28 15:53:37 +0000

    added functionality, styles and animations

--- js/script.js
+++ js/script.js
@@ -1,48 +1,151 @@
 'use strict'
 
-// variable declarations
-const navBar = document.querySelector('nav');
-const header = document.querySelector('header');
+const animatedText = document.querySelector('.animated__info');
+const getStarted = document.querySelector('.get-started')
+const getStartedBtn = document.querySelector('.get-started_btn');
 
-// Event Listeners
-// Linking Navs to content
-navBar.addEventListener('click', function(e) {
-    if(!e.target.classList.contains('nav-list')) return;
+const observerFn = function(entries) {
+    const [entry] = entries;
+    // console.log(entry);
 
-    const id = e.target.getAttribute('data-id');
+    if(entry.isIntersecting) {
+        
+        animatedText.style.opacity = 1;
+        // getStarted.style.opacity = 1;
 
-    const target = document.querySelector(id);
+        // const children = animatedText.querySelectorAll('.animated__info > *');;
+        // children.forEach((child, i) => {
+        //     // console.log(i, child)
+        //     child.classList.add(`anime_${i}`);
+        // })
+    }
+    
+}
 
-    target?.scrollIntoView( {behavior: 'smooth'});
+const observerObj = {root: null, threshold: 0.1}
+
+const textObserver = new IntersectionObserver(observerFn, observerObj);
+textObserver.observe(animatedText)
+// const children = animatedText.querySelectorAll('.animated__info > *');
+// children.forEach(child => textObserver.observe(child))
+
+getStartedBtn.addEventListener('click', function(e) {
+    const sections = document.querySelectorAll('main > *:not(main > *:first-child)');
+    sections.forEach((section, i) => {
+        section.classList.remove('hidden');
+        section.classList.remove('faint');
+    });
+    sections[0].scrollIntoView({behavior: 'smooth'})
+    
 })
 
-// sticky navBar
-// not working, check functionality later
-const callBackFn = function(entries) {
-    const [entry] = entries;
-    if(entry.isIntersecting) return;
 
-    // console.log(entry);
-    // navBar.classList.add('sticky');
-    // console.log('stick');
-}
 
-const optionObserver = {
-    root: null,
-    threshold: 0.1,
-    // rootMargin: 
-}
-const navObserver = new IntersectionObserver(callBackFn, optionObserver);
-navObserver.observe(header);
 
-// let page;
-// window.addEventListener('hashchange', function(e) {
-//     e.preventDefault();
-//     console.log(e.target.location.hash);
-//     page = e.target.location.hash;
-// })
 
-// window.addEventListener('load', function() {
-//     page;
-// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // variable declarations
+// const navBar = document.querySelector('nav');
+// const header = document.querySelector('header');
+
+// // Event Listeners
+// // Linking Navs to content
+// // navBar.addEventListener('click', function(e) {
+// //     if(!e.target.classList.contains('nav-list')) return;
+
+// //     const id = e.target.getAttribute('data-id');
+
+// //     const target = document.querySelector(id);
+
+// //     target?.scrollIntoView( {behavior: 'smooth'});
+// // })
+
+// // sticky navBar
+// // not working, check functionality later
+// const callBackFn = function(entries) {
+//     const [entry] = entries;
+//     if(entry.isIntersecting) return;
+
+//     // console.log(entry);
+//     // navBar.classList.add('sticky');
+//     // console.log('stick');
+// }
+
+// const optionObserver = {
+//     root: null,
+//     threshold: 0.1,
+//     // rootMargin: 
+// }
+// const navObserver = new IntersectionObserver(callBackFn, optionObserver);
+// navObserver.observe(header);
+
+// // let page;
+// // window.addEventListener('hashchange', function(e) {
+// //     e.preventDefault();
+// //     console.log(e.target.location.hash);
+// //     page = e.target.location.hash;
+// // })
+
+// // window.addEventListener('load', function() {
+// //     page;
+// // })
+
+
 
